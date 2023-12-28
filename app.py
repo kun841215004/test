5e297c60b4d91fafdae64bd3c40fcb455ba79b56
kun841215004 <135622725+kun841215004@users.noreply.github.com>
2023-12-28 11:48:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/receive-data', methods=['POST'])
-def receive_data():
-    data = request.json  # 获取JSON数据
-    print(data)  # 打印数据到控制台，或进行其他处理
-    return jsonify({"message": "Data received successfully"}), 200
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
-
-
 
 
 
