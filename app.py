7601f74e72fcc0375561b42efbb5685914991e34
kun841215004 <135622725+kun841215004@users.noreply.github.com>
2023-12-28 16:18:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ received_data = []
 @app.route('/post-data', methods=['POST'])
 def post_data():
     data = request.form.to_dict()  # 获取表单数据
-    print(data)  # 打印数据到控制台
     received_data.append(data)  # 将数据添加到列表中
-    return jsonify({"message": "Form data received"}), 200
+    # 返回一个简单的HTML页面，显示接收到的数据
+    return f"<html><body><h1>Received Data:</h1><pre>{data}</pre></body></html>", 200
 
 @app.route('/show-data')
 def show_data():
-    return jsonify(received_data)  # 返回保存的数据
+    # 以HTML格式返回所有接收到的数据
+    data_html = '<br>'.join([str(data) for data in received_data])
+    return f"<html><body><h1>Received Data:</h1><pre>{data_html}</pre></body></html>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
