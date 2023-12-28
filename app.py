dce868935e238dadb7575a6683266ba276cd44d8
kun841215004 <135622725+kun841215004@users.noreply.github.com>
2023-12-28 16:06:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# 用来存储数据的全局变量
+received_data = []
+
+@app.route('/post-data', methods=['POST'])
+def post_data():
+    data = request.form.to_dict()  # 获取表单数据
+    print(data)  # 打印数据到控制台
+    received_data.append(data)  # 将数据添加到列表中
+    return jsonify({"message": "Form data received"}), 200
+
+@app.route('/show-data')
+def show_data():
+    return jsonify(received_data)  # 返回保存的数据
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
