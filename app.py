07b314fa5561032624fe2f48b92dd7ee280966f3
kun841215004 <135622725+kun841215004@users.noreply.github.com>
2023-12-29 16:14:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-# 用来存储数据的全局变量
+# Global variable to store data
 received_data = []
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/post-data', methods=['GET', 'POST'])
+@app.route('/post-data', methods=['POST'])
 def post_data():
-    if request.method == 'POST':
-        data = request.form.to_dict()  # 获取表单数据
-        received_data.append(data)  # 将数据添加到列表中
-        return f"<html><body><h1>Received Data:</h1><pre>{data}</pre></body></html>", 200
-    else:
-        # 返回一个简单的表单，用于提交数据
-        return '''
-            <html>
-                <body>
-                    <form method="post" action="/post-data">
-                        Data: <input type="text" name="data"><br>
-                        <input type="submit" value="Submit">
-                    </form>
-                </body>
-            </html>
-        '''
-
-@app.route('/show-data')
-def show_data():
-    # 以HTML格式返回所有接收到的数据
-    data_html = '<br>'.join([str(data) for data in received_data])
-    return f"<html><body><h1>Received Data:</h1><pre>{data_html}</pre></body></html>"
+    data = request.form.to_dict()
+    received_data.append(data)
+    socketio.emit('new_data', data)  # Broadcast new data to all clients
+    return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=80)
